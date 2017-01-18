149817d6fd27b26df9dcfe482c5e7adad1e7b0ff
Margaret Thomas <magz.thomas@gmail.com>
2017-01-18 15:21:03 +0200

    Added a directive to generate a button for each answer in the poll.

--- controllers/widgetController.js
+++ controllers/widgetController.js
@@ -1,129 +1,91 @@
 var app = angular.module("widgetApp", ["ui.router"])
-app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/firstState');
+app.config(function($stateProvider) {
     $stateProvider
     .state("first", {
-		url: "/firstState",
+		url: "/firstState",/* Open Poll before voting. */
         templateUrl : "templates/firstState.html"
     })
 	.state("second", {
-		url: "/secondState",
+		url: "/secondState",/* Open Poll after voting. */
 		templateUrl: "templates/secondState.html"
 	})
 	.state("third", {
-		url: "/thirdState",
+		url: "/thirdState",/* Closed Poll. */
 		templateUrl: "templates/thirdState.html"
 	});
 });
-
-app.controller('loadWidget', function($scope){
-	// Determine the state to display for the current user.
+app.controller('loadWidget', function($scope, $state){
+	// Determine the state of the widget to display.
 	$scope.determineState = function(){
-		
+		var pollDetails = JSON.parse(localStorage.getItem("poll"));
+		var pollStatus = pollDetails.pollStatus;
+		console.log(pollStatus);
+		if(pollStatus == "Open And Voted"){
+			$state.go("second");
+		} else if(pollStatus == "Closed"){
+			$state.go("third");
+		}else{
+			$state.go("first");
+		}
 	}
 	// Request data and display in the poll format.
 	$scope.loadOpenPoll = function(){
 		// Dummy content for now. Here is where the the API request will be made.
-		var data = new Array("Open", "1", "Favourite Vegetable", "What is your favourite veggie?", 3,new Array(new Array("Carrots", 10), new Array("Kale", 6), new Array("Cabbage", 4)), 20, "24:00", "Thanks for participating in the Favourite Vegetable Poll!!", "The poll is now closed", "www.google.com");
+		var data = {pollStatus:"Open", pollID:"1", title:"Favourite Vegetable", question:"What is your favourite veggie?", answerCount:3, totalVotes:20, timeLeft:"24:00", thanksMessage:"Thanks for participating in the Favourite Vegetable Poll!!", closedMessage:"The poll is now closed", websiteForSharing:"www.google.com"};
+		var answers = [["Carrots",10], ["Kale",6], ["Cabbage", 4]];
 		
 		// Load stuff from dummy API.
-		$scope.pollStatus = data[0];
-		$scope.pollID = data[1];
-		$scope.pollTitle = data[2];
-		$scope.pollQuestion = data[3];
-		$scope.answerCount = data[4];
-		$scope.totalVotes = data[6];
-		$scope.timeLeft = data[7];
-		$scope.thanksMessage = data[8];
-		$scope.closedMessage = data[9];
-		$scope.websiteForSharing = data[10];
-		
-		// Let's get each possible answer and the answer
-		var arrayOfAnswers = data[5];
-		var allAnswers = [];
-		var allAnswersCounts = [];
-		
-		for(i=0;i<arrayOfAnswers.length;i++){
-			var arr = arrayOfAnswers[i];
-			allAnswers.push(arr[0]);
-			allAnswersCounts.push(arr[1]);
+		$scope.poll = data;
+		$scope.ans = [];
+		$scope.ansVote = [];
+		var index = 0;
+		for(var item in answers){
+			var singleAnswer = answers[item];
+			$scope.ans.push(singleAnswer[0]);
+			$scope.ansVote.push(singleAnswer[1]);
 		}
-		/*
-		// Load answers into the DOM.
-		// Loop through for each answer.
-		for(i=0;i<$scope.answerCount;i++){
-			var btnAnswer = document.createElement("button");
-			var text = document.createTextNode($scope.)
+		// Write dummy data to local storage.
+		localStorage.setItem("poll", JSON.stringify($scope.poll));
+		localStorage.setItem("answers", JSON.stringify(answers));
+	}
+	// Load the data from local storage.
+	$scope.loadState = function(){
+		$scope.poll = JSON.parse(localStorage.getItem("poll"));
+		$scope.chosenAnswer = localStorage.getItem("chosenAnswer");
+		$scope.answers = JSON.parse(localStorage.getItem("answers"));
+	}
+	$scope.formatNumber = function(votes) {
+		return Math.round(votes); 
+	}
+});
+// Creates a button for each answers of the poll.
+app.directive("answers", function($state) {
+    return {
+		restrict: "E",
+		scope:{
+			data: "=" // Current answer.
+		},
+        templateUrl: "button.html",
+		link: function(scope, elem, attrs){
+			elem.bind('click', function(){
+				// Save poll information once the user has voted.
+				var poll = JSON.parse(localStorage.getItem("poll"));
+				poll.totalVotes++;
+				
+				var answers = JSON.parse(localStorage.getItem("answers"));
+				for(var item in answers){
+					var singleAnswer = answers[item];
+					if(singleAnswer[0] == scope.data){
+						singleAnswer[1]++;
+					}
+				}
+				
+				localStorage.setItem("poll", JSON.stringify(poll));
+				localStorage.setItem("answers", JSON.stringify(answers));
+				localStorage.setItem("chosenAnswer", scope.data);
+				
+				$state.go("second");
+			});
 		}
-		*/
-	}
-	// Save poll information once the user has voted.
-	$scope.castVote = function(answer){
-		$scope.totalVotes = $scope.totalVotes + 1;
-		if(answer != null){
-			$scope.chosenAnswer = answer;
-		}else{
-			$scope.chosenAnswer = "Nothing";
-		}
-		document.cookie = "status="+$scope.pollStatus; 
-		document.cookie = "id="+$scope.pollID;
-		document.cookie = "title="+$scope.pollTitle; 
-		document.cookie = "question="+$scope.pollQuestion; 
-		document.cookie = "chosenAnswer="+$scope.chosenAnswer;
-		document.cookie = "count="+$scope.answerCount;
-		document.cookie = "totalVotes="+$scope.totalVotes;
-		document.cookie = "timeLeft="+$scope.timeLeft;
-		document.cookie = "thanks="+$scope.thanksMessage;
-		document.cookie = "closed="+$scope.closedMessage;
-		document.cookie = "website="+$scope.websiteForSharing;
-	}
-	// Load the data from the cookies for the second state.
-	$scope.loadSecondState = function(){
-		$scope.readVote();
-		// Display the chosen answer.
-		//document.getElementById(answer).backgroundColor = pink;
-	}
-	// Load the data from the cookies for the third state.
-	$scope.loadThirdState = function(){
-		$scope.readVote();
-		// Display the chosen answer.
-		//document.getElementById(answer).backgroundColor = pink;
-	}
-	// Read stuff from the coooookie.
-	$scope.readVote = function(){
-		// Check if there is a status cookie.
-		if(document.cookie.indexOf("status") != -1){
-			$scope.pollStatus = $scope.getCookie("status");
-			$scope.pollID = $scope.getCookie("id");
-			$scope.pollTitle = $scope.getCookie("title");
-			$scope.pollQuestion = $scope.getCookie("question");
-			$scope.chosenAnswer = $scope.getCookie("chosenAnswer");
-			$scope.answerCount = $scope.getCookie("count");
-			$scope.totalVotes = $scope.getCookie("totalVotes");
-			$scope.timeLeft = $scope.getCookie("timeLeft");
-			$scope.thanksMessage = $scope.getCookie("thanks");
-			$scope.closedMessage = $scope.getCookie("closed");
-			$scope.websiteForSharing = $scope.getCookie("website");
-		}
-		// Set chosen answer.
-		//console.log($scope.chosenAnswer);
-		/*var btn = document.getElementById($scope.chosenAnswer);
-		btn.style.backgroundColor = "#E6E6E6";*/
-	}
-	
-	$scope.getCookie = function(cname) {
-		var name = cname + "=";
-		var decodedCookie = decodeURIComponent(document.cookie);
-		var ca = decodedCookie.split(';');
-		for(var i = 0; i <ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return "";
-	}
+    };
 });
